babe049a399c937331df00b10f238040ff75c1b4
Riccardo <Riccardo@192.168.162.1>
2019-10-13 21:08:56 +0200

    Inserita funzione di inserimento record

--- src/TabellaAuleDisponibili.java
+++ src/TabellaAuleDisponibili.java
@@ -6,16 +6,17 @@ import javafx.scene.control.cell.PropertyValueFactory;
 
 
 public class TabellaAuleDisponibili {
-    private final TableView tb;
+    private final TableView<Room> tb;
     private ObservableList<Room> ObListRoom;
     TabellaAuleDisponibili(double tbHeigth)
     {        
-        tb = new TableView();
+        tb = new TableView<Room>();
         TableColumn Room = new TableColumn("Room"); 
-        Room.setCellValueFactory(new PropertyValueFactory<>("Room")); 
-        TableColumn Available = new TableColumn("Available"); 
-        Available.setCellValueFactory(new PropertyValueFactory<>("Available")); 
-        TableColumn Booked = new TableColumn("Booked");   
+        Room.setCellValueFactory(new PropertyValueFactory<>("roomName")); 
+        TableColumn Available = new TableColumn("Capacity"); 
+        Available.setCellValueFactory(new PropertyValueFactory<>("capacity")); 
+        TableColumn Booked = new TableColumn("availablePCs");   
+        Booked.setCellValueFactory(new PropertyValueFactory<>("availablePCs")); 
         tb.getColumns().addAll(Room,Available,Booked);
         //---------------D-----------//
         tb.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
