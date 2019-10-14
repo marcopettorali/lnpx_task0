92e5ede665ed2a0c21648fc13a6135fc7939d2d7
Riccardo <Riccardo@192.168.162.1>
2019-10-14 23:27:28 +0200

    Scritto le Tabelle in Inglese

---
+++ src/TableAvailableRooms.java
@@ -0,0 +1,53 @@
+
+import java.util.*;
+import javafx.collections.*;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+
+public class TableAvailableRooms {
+    private TableView<Room> tb;
+    private ObservableList<Room> ObListRoom;
+    TableAvailableRooms(double tbHeigth)
+    {        
+        tb = new TableView<Room>();
+        TableColumn Room = new TableColumn("Room"); 
+        Room.setCellValueFactory(new PropertyValueFactory<>("roomName")); 
+        TableColumn Available = new TableColumn("Capacity"); 
+        Available.setCellValueFactory(new PropertyValueFactory<>("capacity")); 
+        TableColumn Booked = new TableColumn("availablePCs");   
+        Booked.setCellValueFactory(new PropertyValueFactory<>("availablePCs")); 
+        tb.getColumns().addAll(Room,Available,Booked);
+        //---------------D-----------//
+        tb.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+        tb.setPrefHeight(tbHeigth);
+        //--------------------------//
+    }
+    
+    public void FillTableAvailableRooms(List<Room> ArrayRooms)
+    {
+        ObListRoom= FXCollections.observableArrayList();
+        ObListRoom.addAll(ArrayRooms);
+        tb.setItems(ObListRoom);
+        tb.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+    }
+    
+    public Room getSelected(){
+        return tb.getSelectionModel().getSelectedItem();
+    }
+    
+    public void relaseSelection(){
+        tb.getSelectionModel().clearSelection();
+    }
+    
+    public void updateRoomsInformation(int row, Room NewRoomInformations)
+    {
+        ObListRoom.set(row, NewRoomInformations);
+        tb.setItems(ObListRoom);
+    }
+    
+    public TableView gettb()
+    {
+        return tb;
+    }
+}
